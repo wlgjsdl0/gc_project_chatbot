c0ec978ca1f9cae85379f10f794d2e7335e5dccf
mingyu <rlaalsrbgk@gmail.com>
2019-05-28 09:26:30 +0900

    flask model 관련하여 relationship 설정

--- models_flask.py
+++ models_flask.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, Boolean, Date, String, Table, ForeignKey, Float
-from sqlalchemy.orm import relationships, backref
+from sqlalchemy import Column, Integer, String, Table, ForeignKey, Float
+from sqlalchemy.orm import relationship
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,10 +10,41 @@ Sessoion = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
+citys_cctvs_association = Table(
+    'citys_cctvs', Base.metadata,
+    Column('cctv_id', Integer, ForeignKey('cctv.id')),
+    Column('city_id', Integer, ForeignKey('city.id'))
+
+)
+
+
+cityholls_cctvs_association = Table(
+    'cityholls_cctvs', Base.metadata,
+    Column('cctv_id', Integer, ForeignKey('cctv.id')),
+    Column('cityholl_id', Integer, ForeignKey('cityholl.id'))
+
+)
+
+purpose_cctvs_association = Table(
+    'purpose_cctvs', Base.metadata,
+    Column('cctv_id', Integer, ForeignKey('cctv.id')),
+    Column('purpose_id', Integer, ForeignKey('purpose.id'))
+
+)
+
+numbers_cctvs_association = Table(
+    'numbers_cctvs', Base.metadata,
+    Column('cctv_id', Integer, ForeignKey('cctv.id')),
+    Column('number_id', Integer, ForeignKey('phone_number.id'))
+
+)
+
+
 class Cctv(Base):
     __tablename__ = 'cctv'
 
     id = Column(Integer, primary_key=True)
+    city_id = Column(Integer, ForeignKey('city.id'))
     cityholl_id = Column(Integer, ForeignKey('cityholl.id'))
     road_address = Column(String(100), nullable=True)
     address = Column(String(100), nullable=True)
@@ -37,11 +68,23 @@ class Cctv(Base):
         self.data_date = data_date
 
 
+class City(Base):
+    __tablename__ = 'city'
+
+    id = Column(Integer, primary_key=True)
+    city_name = Column(String(100))
+    cctvs = relationship("Cctv", secondary=citys_cctvs_association)
+
+    def __init__(self, city_name):
+        self.city_name = city_name
+
+
 class Cityholl(Base):
     __tablename__ = 'cityholl'
 
     id = Column(Integer, primary_key=True)
     cityholl_name = Column(String(100))
+    cctvs = relationship("Cctv", secondary=cityholls_cctvs_association)
 
     def __init__(self, cityholl_name):
         self.cityholl_name = cityholl_name
@@ -52,6 +95,7 @@ class Purpose(Base):
 
     id = Column(Integer, primary_key=True)
     set_purpose = Column(String(100))
+    cctvs = relationship("Cctv", secondary=purpose_cctvs_association)
 
     def __init__(self, set_purpose):
         self.set_purpose = set_purpose
@@ -62,6 +106,7 @@ class PhoneNumber(Base):
 
     id = Column(Integer, primary_key=True)
     number = Column(String(100))
+    cctvs = relationship("Cctv", secondary=numbers_cctvs_association)
 
     def __init__(self, number):
         self.number = number
